af702804de3d2863f7598879c06debdd1bedfe06
Ayomide05 <gabrieljustina4@gmail.com>
2022-03-11 15:22:35 -0800

    My commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry points
+ * Return: Always (sucess)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */ 
