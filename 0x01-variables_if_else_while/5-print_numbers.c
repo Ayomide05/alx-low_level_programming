edc01090995ce9bb774185d9b0b8166e628738d0
Ayomide05 <gabrieljustina4@gmail.com>
2022-03-11 16:10:34 -0800

    My commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - entry point
+* Return: Always (sucess)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= 'g'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
