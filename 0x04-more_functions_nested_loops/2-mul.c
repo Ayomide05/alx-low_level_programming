ed980fcfacb8fe12f59ed9639494fc583c442fa9
Ayomide05 <gabrieljustina4@gmail.com>
2022-03-17 17:53:44 -0700

    My commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,9 +3,9 @@
  * mul - multiplies two integers
  * @a: The first number to multiply
  * @b: The second number to multiply with the first number
- * Return: The result of the multiplication between x and y
+ * Return: The result of the multiplication between a and b
  */
 int mul(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
