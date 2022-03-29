4bb2d5115a3dd0d733009a9462be2c6c0d063b30
Ayomide05 <gabrieljustina4@gmail.com>
2022-03-28 20:36:45 -0700

    Pointers and Arrays

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,4 +7,24 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int a =	
+	int a =	0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+	return ('\0');
+}
