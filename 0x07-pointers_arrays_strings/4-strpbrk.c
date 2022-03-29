5f9a9933cadabecd72f3ba164517f6c34c901fbc
Ayomide05 <gabrieljustina4@gmail.com>
2022-03-28 20:03:57 -0700

    Pointers and Arrays

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strpbrk - search for string for any set of bytes
+ * @s: source string
+ * @accept: accepted characters
+ * Return: the sting since the first found accepted character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a =	
